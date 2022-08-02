f6c009dd00c9a481d40f6f18cb600a2e9366d9bc
john barnett <johnkbarnett11@msn.com>
2022-08-01 18:40:38 -0600

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by John Barnett on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
